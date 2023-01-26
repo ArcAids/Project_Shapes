7bc04def90c2edf0c1196760fa12717f6fa56ddf
Arpit Sharma <48210246+ArcAids@users.noreply.github.com>
2023-01-25 20:46:36 -0600

    Circles
    Added Json read
    
    shape data structure.
    circle shape is in.
    
    Should I add some security with public class or keep it simple..?
    
    TODO:
    add write to file.
    add other shapes

--- Project_Shapes/Program.cs
+++ Project_Shapes/Program.cs
@@ -6,7 +6,17 @@ namespace Project_Shapes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ShapeDataFileIOBase dataFileIO = new ShapeDataFileIOBase();
+            var data = dataFileIO.ReadFile("../../../../Shapes-49464.json");
+
+            foreach (ShapeBase circle in data.Circles)
+            {
+                circle.Print();
+                Console.WriteLine("Area is:" + circle.GetArea().ToString());
+                Console.WriteLine("Perimeter is:" + circle.GetPerimeter().ToString());
+            }
+
+
         }
     }
 }
---
+++ Project_Shapes/Shapes/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Project_Shapes
+{
+    public class Circle : GenericShape
+    {
+        public float Radius { get; set; }
+
+        public Circle()
+        {
+            Radius = 0;
+        }
+
+        public Circle(int ID, float centerX, float centerY, float radius) : base(ID, centerX, centerY)
+        {
+            this.Radius = radius;
+        }
+
+        public override float GetArea()
+        {
+            return MathF.PI * Radius * Radius;
+        }
+
+        public override float GetPerimeter()
+        {
+            return MathF.PI * 2 * Radius;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Circle:");
+            base.Print();
+            Console.WriteLine(" | Radius: " + Radius.ToString());
+        }
+    }
+}
